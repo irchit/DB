424ede344c0c9d11ee505329f93a22a927746b99
Chițea Răzvan <rchitea@gmail.com>
2023-11-07 05:37:44 +0200

    Finished Lab2

--- lab2_interogari_tabele.sql
+++ lab2_interogari_tabele.sql
@@ -1,14 +1,14 @@
-USE [DBDogs]
+﻿USE [DBDogs]
 
 /*
-min 5 where - 4
-min 3 group by - 3 ok
-min 2 distinct - 1
-min 2 having - 1
-min 7 ? from min 2 tables - 4
-min 2 ? from m-n tables - 3
+min 5 where - 5 ok
+min 3 group by - 4 ok
+min 2 distinct - 2 ok
+min 2 having - 2 ok
+min 7 ? from min 2 tables - 7 ok 
+min 2 ? from m-n tables - 4 ok
 
-exactly 10 ? - 7
+exactly 10 ? - 10 ok
 */
 
 -- 1. Extrage toti cainii care sunt maidanezi
@@ -81,3 +81,50 @@ WHERE
 	)
 GROUP BY L.tara
 ORDER BY [Average Airedale Terrier Price];
+
+-- 8. Numar de Caini cu minim 65% culoare neagra
+SELECT
+	CONVERT(VARCHAR, CC.procent_culoare*100) + '%' as [Color %],
+	COUNT(C.id) as [Number of Black Dogs that are minimum 65% black]
+FROM Caine C
+JOIN CuloareCaine CC ON C.id = CC.id_caine
+JOIN Culoare Col ON Col.id = CC.id_culoare
+WHERE
+	CC.id_culoare = (
+		SELECT Col.id 
+		FROM Culoare Col 
+		WHERE 
+			Col.culoare = 'negru'
+	)
+GROUP BY CC.procent_culoare
+HAVING CC.procent_culoare >= 0.65
+ORDER BY CC.procent_culoare DESC;
+
+-- 9. Lista de Rase si culori in procent distincte (pentru a obtine filtre)
+SELECT DISTINCT 
+	R.denumire as Breed,
+	Col.culoare as Color,
+	CONVERT(VARCHAR, CC.procent_culoare*100) + '%' as [Color %]
+FROM Caine C
+JOIN Rasa R ON C.rasa_id = R.id
+JOIN CuloareCaine CC ON CC.id_caine = C.id
+JOIN Culoare Col ON Col.id = CC.id_culoare
+ORDER BY COLOR DESC, [Color %] DESC, Breed DESC;
+
+-- 10. Toate Marcile De Hrana cu un scor strict mai mare ca 6 pentru Talie medie care sa fie de vita sau pui
+SELECT
+	H.denumire as Name,
+	H.carne as [Meat Type],
+	H.calitate as [Grade out of 10],
+	H.varsta as Age
+FROM Hrana H
+WHERE
+	H.talie_id = (
+		SELECT T.id 
+		FROM Talie T 
+		WHERE T.tip = 'medie'
+	) AND
+	H.calitate > 6 AND
+	(H.carne = 'vită' OR H.carne = 'pui')
+ORDER BY H.carne ASC, H.calitate DESC;
+
