5e82fadf1302f174decd1756cb55fcde90cc613b
Chițea Răzvan <rchitea@gmail.com>
2023-11-07 02:59:36 +0200

    am facut cafeaua, acum intru turbo

--- lab2_interogari_tabele.sql
+++ lab2_interogari_tabele.sql
@@ -1,5 +1,16 @@
 USE [Dogs]
 
+/*
+min 5 where - 3
+min 3 group by - 1
+min 2 distinct - 1
+min 2 having - 2
+min 7 ? from min 2 tables - 2
+min 2 ? from m-n tables - 0
+
+exactly 10 ? - 2
+*/
+
 -- Extrage toti cainii care sunt maidanezi
 SELECT * FROM [Caini] WHERE [rasa_id] = (SELECT [id] FROM [Rasa] WHERE [denumire] = 'Maidanez')
 
